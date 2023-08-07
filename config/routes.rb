47821273cebaefaaf405538b873451c3cdbafd6b
RajdeepSrivastava <rajmymail36@gmail.com>
2023-08-07 18:59:59 +0530

    Event and Users

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  root "events#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+  
+  resources :events, only: [:index, :new, :create, :show]
+
+  patch '/events/:id', to: 'events#update'
+  
+  delete '/events/:id', to: 'events#destroy'
+  
+end
